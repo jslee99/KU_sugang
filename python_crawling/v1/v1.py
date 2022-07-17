cd49c8ef4365abec375b7b840b3ec590a687f0b0
jslee99 <junsub_lee@naver.com>
2022-07-17 18:08:48 +0900

    first

---
+++ python_crawling/v1/v1.py
@@ -0,0 +1,10 @@
+import pymysql
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
+
+def main():
+    print('yes')
+
+if __name__ == '__main__':
+    main()
